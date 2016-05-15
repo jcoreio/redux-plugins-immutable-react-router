37572730b1cd8f0dfc2db21b738ec184f3fc3aeb
Andy Edwards <jedwards@fastmail.com>
2016-05-15 16:26:47 -0500

    try and fail to refresh route when plugins change

--- src/Router.js
+++ src/Router.js
@@ -22,6 +22,42 @@ export default class Router extends Component<void, Props, void> {
     store: PropTypes.object
   };
 
+  router: ?Object;
+  lastPlugins: any;
+  unsubscribe: ?Function;
+
+  componentDidMount(): void {
+    const store = this.props.store || this.context.store
+    if (store) this.unsubscribe = store.subscribe(this.onStateChange)
+  }
+
+  componentWillReceiveProps(nextProps: Props, nextContext: any): void {
+    const store = this.props.store || this.context.store
+    const nextStore = nextProps.store || nextContext.store
+    if (store !== nextStore) {
+      if (this.unsubscribe) this.unsubscribe()
+      if (store) this.unsubscribe = store.subscribe(this.onStateChange)
+      else this.unsubscribe = null
+    }
+  }
+
+  componentWillUnmount(): void {
+    if (this.unsubscribe) this.unsubscribe()
+  }
+
+  onStateChange: Function = () => {
+    const {router} = this;
+    const store = this.props.store || this.context.store
+    if (store && router) {
+      const {lastPlugins} = this
+      const nextPlugins = this.lastPlugins = store.getState().get('plugins')
+      if (lastPlugins !== nextPlugins) {
+        console.log("TEST")
+        router.refresh()
+      }
+    }
+  };
+
   // cache routes so that we don't pass a new copy of the same routes to ReactRouter.
   // if we passed the result of createRouteDecorator(store) every time, it would be a new object and react-router
   // would warn that the routes shouldn't be changed.  However, if the user does change the routes, this will make
@@ -33,6 +69,7 @@ export default class Router extends Component<void, Props, void> {
   );
 
   render(): React.Element {
-    return <ReactRouter {...this.props} routes={this.selectRoutes(this.props, this.context)} children={null} />
+    return <ReactRouter {...this.props} ref={c => this.router = c}
+                                        routes={this.selectRoutes(this.props, this.context)} children={null} />
   }
 }
